8970261b24eec3bf0681963f04d6eb905a81c3f2
გიორგი ანთია <giorgi.antia@softgen.ge>
2024-09-25 12:12:04 +0400

    feat: remove username from CreateCommentDto

--- src/track/dto/create-comment.dto.ts
+++ src/track/dto/create-comment.dto.ts
@@ -2,10 +2,6 @@ import { ObjectId } from 'mongoose';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCommentDto {
-  @IsString({ message: 'Username must be a string' })
-  @IsNotEmpty({ message: 'Username is required!' })
-  readonly username: string;
-
   @IsString({ message: 'Text must be a string' })
   @IsNotEmpty({ message: 'Text is required!' })
   readonly text: string;
